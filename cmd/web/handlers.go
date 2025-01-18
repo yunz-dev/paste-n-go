a7920bca33f5b9787172a52b64c83cd254711c37
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-01-19 00:57:31 +1100

    refactor: made homepage invoke for base, title and body

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -16,7 +16,12 @@ func home(w http.ResponseWriter, r *http.Request) {
     return
     }
 
-  ts, err := template.ParseFiles("./ui/html/home.page.html")
+  files := []string{
+    "./ui/html/home.page.tmpl",
+    "./ui/html/base.layout.tmpl",
+  }
+
+  ts, err := template.ParseFiles(files...)
   if err != nil {
     log.Println(err.Error())
     http.Error(w, "Internal Server Error", 500)
