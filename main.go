612df7bff62a3482e39613f7321e2e64f16350e7
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-01-18 12:25:40 +1100

    DOCS: Added comments for server, handler, and mux

--- main.go
+++ main.go
@@ -5,15 +5,18 @@ import (
   "net/http"
 )
 
+// handler for home '/'
 func home(w http.ResponseWriter, r *http.Request) {
   w.Write([]byte("Hello from Paste n Go"))
 }
 
 func main() {
+  // initialise new router ServeMux
   mux := http.NewServeMux()
   mux.HandleFunc("/", home)
 
   log.Println("Starting Server on port :4000")
+  // initialise web server
   err := http.ListenAndServe(":4000", mux)
   log.Fatal(err)
 
