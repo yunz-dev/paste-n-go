d8a0b6c76c845abb2986255d3cdf7b3b3fd4dc74
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-01-19 00:39:13 +1100

    feat: html template for main page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,6 +4,8 @@ import (
   "fmt"
   "net/http"
   "strconv"
+  "log"
+  "html/template"
 )
 
 // handler for home '/'
@@ -14,7 +16,19 @@ func home(w http.ResponseWriter, r *http.Request) {
     return
     }
 
-  w.Write([]byte("Hello from Paste n Go"))
+  ts, err := template.ParseFiles("./ui/html/home.page.html")
+  if err != nil {
+    log.Println(err.Error())
+    http.Error(w, "Internal Server Error", 500)
+    return
+  }
+
+  err = ts.Execute(w, nil)
+  if err != nil {
+    log.Println(err.Error())
+    http.Error(w, "Internal Server Error", 500)
+  }
+  // w.Write([]byte("Hello from Paste n Go"))
 }
 
 // TODO: implement
