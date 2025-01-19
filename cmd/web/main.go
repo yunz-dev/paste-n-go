58c2d788b080fd46e0b05cd8d926de9cab276988
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-01-19 22:03:08 +1100

    refactor: added info and error logs as well as making custom httpServer strucgt

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,10 @@
 package main
 
-import(
-  "flag"
-  "log"
-  "net/http"
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
 )
 
 type Config struct {
@@ -16,8 +17,18 @@ func main() {
   flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP network address")
   flag.Parse()
   fileServer := http.FileServer(http.Dir("./ui/static"))
+
+  infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+  errLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
   // initialise new router ServeMux
   mux := http.NewServeMux()
+
+  srv := &http.Server{
+    Addr: cfg.Addr,
+    ErrorLog: errLog,
+    Handler: mux,
+  }
   // add handler functions
   mux.HandleFunc("/", home)
   mux.HandleFunc("/snippet", showSnippet)
@@ -27,9 +38,9 @@ func main() {
   // NOTE: ListenAndServe follows host:port
   // if no host then it will listen all all
   // hosts available on the computer
-  log.Printf("Starting Server on port %s", cfg.Addr)
+  infoLog.Printf("Starting Server on port %s", cfg.Addr)
   // initialise web server
-  err := http.ListenAndServe(cfg.Addr, mux)
-  log.Fatal(err)
+  err := srv.ListenAndServe()
+  errLog.Fatal(err)
 
 }
